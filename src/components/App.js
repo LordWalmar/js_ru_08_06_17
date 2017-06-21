2058d7794372021cb06c9d161e44a85bcadd2337
LordWalmar <LordWalmar@ya.ru>
2017-06-21 21:36:28 +0300

    feature: added day-select-picker

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
 import UserForm from './UserForm'
 import Select from 'react-select'
+import DayPicker, { DateUtils } from 'react-day-picker'
 import 'react-select/dist/react-select.css'
+import 'react-day-picker/lib/style.css';
 
 class App extends Component {
     static propTypes = {
@@ -12,10 +14,18 @@ class App extends Component {
     };
 
     state = {
-        selection: null
+        selection: null,
+        from: null,
+        to: null
     }
 
+    handleDayClick = day => {
+        const range = DateUtils.addDayToRange(day, this.state);
+        this.setState(range);
+    };
+
     render() {
+        const {from, to, selection} = this.state
         const {articles} = this.props
         const options = articles.map(article => ({
             label: article.title,
@@ -25,7 +35,16 @@ class App extends Component {
         return (
             <div>
                 <UserForm />
-                <Select options = {options} value = {this.state.selection} onChange = {this.changeSelection} multi />
+                <Select options = {options} value = {selection} onChange = {this.changeSelection} multi />
+                <DayPicker
+                    numberOfMonths={2}
+                    selectedDays={[from, { from, to }]}
+                    onDayClick={this.handleDayClick}
+                    fixedWeeks
+                />
+                { from !== null && to !== null &&
+                    <p>{`You chose from ${from.toISOString().slice(0, 10)} to ${to.toISOString().slice(0, 10)}.`}</p>
+                }
                 <ArticleList articles = {articles} defaultOpenId = {articles[0].id}/>
                 <ArticlesChart articles = {articles} />
             </div>
