954ea725b89417055f263c03aaba10bfefcc1574
LordWalmar <LordWalmar@ya.ru>
2017-07-09 01:21:03 +0300

    feature: added HW-6

--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,15 +1,35 @@
-import {normalizedComments as defaulComments} from '../fixtures'
-import {ADD_COMMENT} from '../constants'
+import {ADD_COMMENT, LOAD_ARTICLE_COMMENTS, SUCCESS, FAIL} from '../constants'
+import {OrderedMap, Record} from 'immutable'
 import {arrToMap} from '../helpers'
 
-const commentsMap = arrToMap(defaulComments)
+const defaulComments = Record({
+    entities: new OrderedMap({})
+})
 
-export default (commentsState = commentsMap, action) => {
-    const {type, payload, randomId} = action
+const defaultState = new defaulComments()
+
+const CommentRecord = Record({
+    id: undefined,
+    user: undefined,
+    text: undefined
+})
+
+export default (commentsState = defaultState, action) => {
+    const {type, payload, randomId, response} = action
 
     switch (type) {
         case ADD_COMMENT:
-            return {...commentsState, [randomId]: payload.comment}
+            return commentsState.updateIn(
+                ['entities', randomId],
+                comment => new CommentRecord({...payload.comment, id: randomId})
+            )
+
+        case LOAD_ARTICLE_COMMENTS + SUCCESS:
+            return commentsState
+                .set('entities', arrToMap(payload.response, CommentRecord))
+
+        case LOAD_ARTICLE_COMMENTS + FAIL:
+            console.log('payload.error', payload.error)
     }
 
     return commentsState
